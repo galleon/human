fd973d82e689b63acbc42aec619227b505f88e3a
James Moultrie <82656270+mayze13@users.noreply.github.com>
2023-03-13 12:42:23 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,6 @@ import numpy as np
 import pretty_midi
 import requests
 import streamlit as st
-from bs4 import BeautifulSoup
 from scipy.io import wavfile
 
 st.markdown(f"<h1 style='text-align: center;'> 🤖 Music Generator 🤖 </h1>", unsafe_allow_html=True)
