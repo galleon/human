52ef9fcbf6672c69ba6caf95115ce0703c0599d6
James Moultrie <82656270+mayze13@users.noreply.github.com>
2023-03-14 11:21:00 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -9,7 +9,7 @@ from scipy.io import wavfile
 st.set_page_config(
         page_title="MIDI to WAV",
         page_icon="musical_note",
-        initial_sidebar_state="collapsed")
+        initial_sidebar_state="expanded")
 st.markdown(f"<h1 style='text-align: center;'> 🤖 Music Generator 🤖 </h1>", unsafe_allow_html=True)
 style = st.sidebar.selectbox(
         "🎹 Select your style 🎸",
@@ -31,9 +31,9 @@ params = {
 "nb_bars":bars,
 "temperature":temperature
 }
-
-with st.spinner(f"Transcribing to FluidSynth"):
+with st.spinner(f"Fetching Request"):
         response = requests.get(url, params).json()
+with st.spinner(f"Transcribing to FluidSynth"):
         midi_data = pretty_midi.PrettyMIDI(response)
         audio_data = midi_data.fluidsynth()
         audio_data = np.int16(
